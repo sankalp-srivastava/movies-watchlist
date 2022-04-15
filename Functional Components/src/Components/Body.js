6e69be9ebba5af187636281662d1eece29a49e3d
Sankalp Srivastava <raunaksrivastava22@gmail.com>
2022-04-15 23:41:51 +0530

    fixed styles of radio and checkbox buttons

--- Functional Components/src/Components/Body.js
+++ Functional Components/src/Components/Body.js
@@ -99,7 +99,7 @@ const BpIcon2 = styled('span')(({ theme }) => ({
     },
   }));
 
-const BpCheckedIcon = styled(BpIcon)({
+const BpCheckedIcon2 = styled(BpIcon)({
     backgroundColor: '#137cbd',
     backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
     borderRadius:"50%",
@@ -119,14 +119,17 @@ const BpCheckedIcon = styled(BpIcon)({
     },
 });
 
-const BpCheckedIcon2 = styled(BpIcon)({
+const BpCheckedIcon = styled(BpIcon)({
     backgroundColor: '#137cbd',
     backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
     '&:before': {
       display: 'block',
       width: 16,
       height: 16,
-      backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
+      backgroundImage:
+        "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+        " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+        "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
       content: '""',
     },
     'input:hover ~ &': {
